30946c24a89ab3b9946c943f4c423cae4c1c6a37
Maria Muntean <MunteanDMaria@gmail.com>
2024-07-06 21:48:56 -0400

    moved out of the folder.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+import anime from js/anime.js
 
+function increaseFn() { 
+    var btn = document.getElementById("myButton"); 
+    var currwidth = btn.clientWidth; 
+    var currheight = btn.clientHeight; 
+    btn.style.width = 500 + "px"; 
+    btn.style.height = 500 + "px"; 
+
+
+
+    //btn.style.width = (currwidth + 20) + "px"; 
+    //btn.style.height = (currheight + 10) + "px"; 
+} 
