238a44b28e6456a70f3e3e33e143945f23632024
Maria Muntean <MunteanDMaria@gmail.com>
2024-08-19 09:38:08 -0400

    button to go to avatar page

---
+++ js/Avatar.js
@@ -0,0 +1,13 @@
+
+function increaseFuuuuuun() { 
+    var btn = document.getElementById("myButton"); 
+    var currwidth = btn.clientWidth; 
+    var currheight = btn.clientHeight; 
+    btn.style.width = 500 + "px"; 
+    btn.style.height = 500 + "px"; 
+
+
+
+    //btn.style.width = (currwidth + 20) + "px"; 
+    //btn.style.height = (currheight + 10) + "px"; 
+} 
